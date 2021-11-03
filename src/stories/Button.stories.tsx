fbd63d70075b5373397714266019a0fc6c99fc42
gurdilou <thomas.luce7342@gmail.com>
2021-11-03 10:26:29 +0100

    Add repro example

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
+// @ts-ignore
+import CustomDoc from "./Button.doc.mdx";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: {
+      control: 'color',
+      description: "**An overriden description in .stories.tsx file**"
+    },
   },
+  parameters: {
+    docs: {
+      page: CustomDoc
+    },
+    controls: {
+      expanded: "true"
+    }
+  }
+
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
   label: 'Button',
